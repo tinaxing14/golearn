890bafc525d00c0b1e340b1a4ef5b08f16bb2d4d
txing <txing@caring.com>
2020-11-16 12:48:10 -0500

    add map[string]interface{} example

--- index.go
+++ index.go
@@ -27,3 +27,31 @@ func main() {
 func changeValue(x *int) {
 	*x = 7
 }
+
+// map[string]interface{} - decoding unknown JSON data to map[string]interface{}:
+
+// Known Person object
+type Person struct {
+	Name    string
+	Age     int
+	Hobbies []string
+}
+
+var person1 Person
+
+var person2 map[string]interface{}
+
+// Do a type switch do check what type of value is in the unknown object
+
+func check(p map[string]interface{}) {
+	for k, v := range p {
+		switch c := v.(type) {
+		case string:
+			fmt.Printf("item %q is a string containing %q\n, k, c")
+		case float64:
+			fmt.Printf("item %q is a number")
+		default:
+			fmt.Printf("not sure the type, might be %T\n", k, c)
+		}
+	}
+}
